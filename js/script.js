8d2e0c1b3faaa3f3e8a8d117ea149119db344558
Alexander Krasnoselskikh <selskih@mail.ru>
2023-08-26 21:38:03 +0300

    Playfield

--- js/script.js
+++ js/script.js
@@ -1,49 +1,37 @@
-$(function () {
-  $("#navbar-toggler-button").blur(function (event) {
-    var screenWidth = window.innerWidth;
-    if (screenWidth < 768) {
-      $("#navbar-toggler").collapse('hide');
-    }
-  });
-});
+﻿var alphabetSize = 46;
+var alphabetShifts = [
+  1, 1, 1, 1, 0, // a
+  1, 1, 1, 1, 1, // ka
+  1, 1, 1, 1, 1, // sa
+  1, 2, 1, 1, 1, // ta
+  0, 0, 0, 0, 0, // na
+  2, 2, 2, 2, 2, // ha
+  0, 0, 0, 0, 1, // ma
+  1, 1, 0, 0, 0, // ra
+  0, 0, 1, 2, 0, // wa
+  0              // n
+];
 
-function sendGetRequest(url, responseHandler) {
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function () {
-    handleResponse(request, responseHandler);
-  };
-  request.open("GET", url);
-  request.send(null);
-}
+var grid = document.getElementById('grid');
+var hierogpyph = 'あ';
+for (var i = 0; i < alphabetSize;) {
+  var row = document.createElement('div');
+  row.setAttribute('class', 'row row-cols-5');
+  row.style.marginBottom = '4px';
+  grid.appendChild(row);
 
-function handleResponse(request, responseHandler) {
-  if (request.readyState == 4 && request.status == 200) {
-    console.log(request);
-    responseHandler(request);
+  for (var c = 0; c < 5 && i < alphabetSize; c++, i++) {
+    var col = document.createElement('div');
+    col.setAttribute('class', 'col');
+    col.style.display = 'flex';
+    col.style.justifyContent = 'center';
+    row.appendChild(col);
+
+    var button = document.createElement('button');
+    button.innerHTML = hierogpyph;
+    button.setAttribute('class', 'button-grid');
+    button.setAttribute('role', 'button');
+    hierogpyph = String.fromCharCode(hierogpyph.charCodeAt(0) + 1 + (i < alphabetShifts.length ? alphabetShifts[i] : 0));
+    col.appendChild(button);
   }
 }
-
-(function (global) {
-  var homeHtml = "snippets/home-main.html";
-  var dc = {};
-
-  var insertHtml = function (selector, html) {
-    var target = document.querySelector(selector);
-    target.innerHTML = html;
-  };
-
-  var showLoading = function (selector) {
-    var html = "<div class='text-center'>";
-    html += "<img src='img/ajax-loader.gif'>";
-    html += "</div>";
-    insertHtml(selector, html);
-  };
-
-  document.addEventListener("DOMContentLoaded", function (event) {
-    showLoading("#main");
-
-    sendGetRequest(homeHtml, function (request) {
-      document.querySelector("#main").innerHTML = request.responseText;
-    });
-  });
-})(window);
