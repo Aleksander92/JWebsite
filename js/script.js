4206f31e46a0cbbae8656fbd8896061b2ec5943f
Alexander Krasnoselskikh <selskih@mail.ru>
2023-09-06 10:03:17 +0300

    Game requests and statistics output

--- js/script.js
+++ js/script.js
@@ -31,7 +31,58 @@ for (var i = 0; i < alphabetSize;) {
     button.innerHTML = hierogpyph;
     button.setAttribute('class', 'button-grid');
     button.setAttribute('role', 'button');
-    hierogpyph = String.fromCharCode(hierogpyph.charCodeAt(0) + 1 + (i < alphabetShifts.length ? alphabetShifts[i] : 0));
+    button.onclick = function () { checkAnswerAndMakeNewTask(this) };
     col.appendChild(button);
+
+    hierogpyph = String.fromCharCode(hierogpyph.charCodeAt(0) + 1 + (i < alphabetShifts.length ? alphabetShifts[i] : 0));
   }
 }
+
+async function fetchUrlGet(url) {
+  var response = await fetch(url, {
+    method: 'GET',
+    mode: 'cors',
+  })
+    .then(response => response.json());
+  console.log(response);
+  return response;
+}
+
+async function fetchAndSet(url, docField, urlFields) {
+  response = await fetchUrlGet(url);
+
+  console.log(urlFields);
+  console.log(typeof urlFields);
+  if (typeof urlFields === 'string') {
+    console.log('string');
+    console.log(urlFields);
+    response = response[urlFields];
+  } else {
+    for (urlField of urlFields) {
+      console.log('object');
+      console.log(urlField);
+      response = response[urlField];
+    }
+  }
+
+  document.getElementById('out1')[docField] = response;
+}
+
+async function createGame() {
+  response = await fetchUrlGet('https://localhost:7073/create_game?mode=1');
+
+  document.getElementById('attempts').textContent = response['attempts'];
+  document.getElementById('correct-attempts').textContent = response['correctAttempts'];
+  document.getElementById('correct-attempts-percentage').textContent = response['attempts'] == 0 ? 100 : response['correct-attempts'] / resposne['attempts'];
+}
+
+async function checkAnswerAndMakeNewTask(button) {
+  console.log(button.textContent);
+  response = await fetchUrlGet(`https://localhost:7073/check_answer_and_make_new_task?id=1&userAnswer=${button.textContent}`);
+}
+
+//fetchAndSet('https://jsonplaceholder.typicode.com/posts/1', 'textContent', 'body');
+
+//fetchAndSet('https://localhost:7073/states', 'textContent', [0, 'id']);
+
+createGame();
