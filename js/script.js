e85f86f2a49c144651751c1efef052a79f44eaa3
Alexander Krasnoselskikh <selskih@mail.ru>
2023-09-09 22:49:54 +0300

    Texts, buttons, grid generation, animations

--- js/script.js
+++ js/script.js
@@ -1,42 +1,73 @@
-﻿var alphabetSize = 46;
-var alphabetShifts = [
-  1, 1, 1, 1, 0, // a
-  1, 1, 1, 1, 1, // ka
-  1, 1, 1, 1, 1, // sa
-  1, 2, 1, 1, 1, // ta
-  0, 0, 0, 0, 0, // na
-  2, 2, 2, 2, 2, // ha
-  0, 0, 0, 0, 1, // ma
-  1, 1, 0, 0, 0, // ra
-  0, 0, 1, 2, 0, // wa
-  0              // n
+﻿//////////////////////////////////////////////////////////////////////////////////////////////
+//                          Grid generation and alphabet functions
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var alphabet = [
+  ['あ', 'a'], ['い', 'i'], ['う', 'u'], ['え', 'e'], ['お', 'o'],
+  ['か', 'ka'], ['き', 'ki'], ['く', 'ku'], ['け', 'ke'], ['こ', 'ko'],
+  ['さ', 'sa'], ['し', 'shi'], ['す', 'su'], ['せ', 'se'], ['そ', 'so'],
+  ['た', 'ta'], ['ち', 'chi'], ['つ', 'tsu'], ['て', 'te'], ['と', 'to'],
+  ['な', 'na'], ['に', 'ni'], ['ぬ', 'nu'], ['ね', 'ne'], ['の', 'no'],
+  ['は', 'ha'], ['ひ', 'hi'], ['ふ', 'fu'], ['へ', 'he'], ['ほ', 'ho'],
+  ['ま', 'ma'], ['み', 'mi'], ['む', 'mu'], ['め', 'me'], ['も', 'mo'],
+  ['や', 'ya'], ['ゆ', 'yu'], ['よ', 'yo'],
+  ['ら', 'ra'], ['り', 'ri'], ['る', 'ru'], ['れ', 're'], ['ろ', 'ro'],
+  ['わ', 'wa'], ['を', 'wo'], ['ん', 'n']
+];
+var gaps = [
+  36, 37, 44, 45
 ];
 
+function hiraganaToEnglish(hieroglyph) {
+  for (item of alphabet) {
+    if (item[0] == hieroglyph) {
+      return item[1];
+    }
+  }
+  throw new Error(`No such hieroglyph: ${hieroglyph}`);
+}
+
 var grid = document.getElementById('grid');
-var hierogpyph = 'あ';
-for (var i = 0; i < alphabetSize;) {
+var gapUsed = false;
+for (var i = 0; i < alphabet.length;) {
   var row = document.createElement('div');
   row.setAttribute('class', 'row row-cols-5');
   row.style.marginBottom = '4px';
   grid.appendChild(row);
 
-  for (var c = 0; c < 5 && i < alphabetSize; c++, i++) {
+  for (var c = 0; c < 5 && i < alphabet.length; c++, i++) {
     var col = document.createElement('div');
     col.setAttribute('class', 'col');
     col.style.display = 'flex';
     col.style.justifyContent = 'center';
     row.appendChild(col);
 
+    if (gaps.includes(i) && !gapUsed) {
+      --i;
+      gapUsed = true;
+      continue;
+    }
+    gapUsed = false;
+
     var button = document.createElement('button');
-    button.innerHTML = hierogpyph;
-    button.setAttribute('class', 'button-grid');
+    button.innerHTML = alphabet[i][0];
+    button.setAttribute('class', 'button-grid glow');
+    button.setAttribute('id', 'button-grid-' + alphabet[i][1]);
     button.setAttribute('role', 'button');
     button.onclick = function () { checkAnswerAndMakeNewTask(this) };
+    button.addEventListener('animationstart', () => { });
+    button.addEventListener('animationiteration', () => { });
+    button.addEventListener('animationend', function(e) {
+      this.classList.remove('class-glow-correct');
+      this.classList.remove('class-glow-incorrect');
+    });
     col.appendChild(button);
-
-    hierogpyph = String.fromCharCode(hierogpyph.charCodeAt(0) + 1 + (i < alphabetShifts.length ? alphabetShifts[i] : 0));
   }
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//   End                    Grid generation and alphabet functions
+//////////////////////////////////////////////////////////////////////////////////////////////
 
 async function fetchUrlGet(url) {
   var response = await fetch(url, {
@@ -73,18 +104,22 @@ async function createGame() {
 
   localStorage.setItem('id', response['id']);
 
-  setOutput(response);
+  processResponse(response);
 }
 
 async function checkAnswerAndMakeNewTask(button) {
-  console.log(button.textContent);
-  console.log(localStorage.getItem('id'));
+  localStorage.setItem('userAnswer', button.textContent);
+
   response = await fetchUrlGet(`https://localhost:7073/check_answer_and_make_new_task?id=${localStorage.getItem('id')}&userAnswer=${button.textContent}`);
 
-  setOutput(response);
+  highlightGridButton(response['prevTaskAnswer']);
+
+  processResponse(response);
 }
 
-async function setOutput(response) {
+async function processResponse(response) {
+  localStorage.setItem('taskQuestion', response['taskQuestion']);
+
   document.getElementById('task-question').textContent = response['taskQuestion'];
   document.getElementById('attempts').textContent = response['attempts'];
   document.getElementById('correct-attempts').textContent = response['correctAttempts'];
@@ -96,3 +131,10 @@ async function setOutput(response) {
 //fetchAndSet('https://localhost:7073/states', 'textContent', [0, 'id']);
 
 createGame();
+
+function highlightGridButton(prevTaskAnswer) {
+  document.getElementById('button-grid-' + localStorage.getItem('taskQuestion')).classList.toggle('class-glow-correct');
+  if (localStorage.getItem('userAnswer') != prevTaskAnswer) {
+    document.getElementById('button-grid-' + hiraganaToEnglish(localStorage.getItem('userAnswer'))).classList.toggle('class-glow-incorrect');
+  }
+}
