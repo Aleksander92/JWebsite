1b9aeaf594d6407966e028a11ad8896292982322
Alexander Krasnoselskikh <selskih@mail.ru>
2023-09-10 22:48:03 +0300

    Add hiragana to english mode

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 ﻿//////////////////////////////////////////////////////////////////////////////////////////////
-//                          Grid generation and alphabet functions
+//                       Grid generation and alphabet working functions
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 var alphabet = [
@@ -27,46 +27,62 @@ function hiraganaToEnglish(hieroglyph) {
   throw new Error(`No such hieroglyph: ${hieroglyph}`);
 }
 
-var grid = document.getElementById('grid');
-var gapUsed = false;
-for (var i = 0; i < alphabet.length;) {
-  var row = document.createElement('div');
-  row.setAttribute('class', 'row row-cols-5');
-  row.style.marginBottom = '4px';
-  grid.appendChild(row);
+function anythingToEnglish(s) {
+  if (s[0] >= 'a' && s[0] <= 'z') {
+    return s;
+  }
+  return hiraganaToEnglish(s);
+}
 
-  for (var c = 0; c < 5 && i < alphabet.length; c++, i++) {
-    var col = document.createElement('div');
-    col.setAttribute('class', 'col');
-    col.style.display = 'flex';
-    col.style.justifyContent = 'center';
-    row.appendChild(col);
+async function createGrid(mode) {
+  var grid = document.getElementById('grid');
+  var gapUsed = false;
+  var alphabetTextContentIndex = (mode ^ 1);
 
-    if (gaps.includes(i) && !gapUsed) {
-      --i;
-      gapUsed = true;
-      continue;
+  var rows = document.getElementsByClassName('grid-row');
+  for (i = rows.length - 1; i > -1; --i) {
+    rows[i].remove();
+  }
+
+  for (var i = 0; i < alphabet.length;) {
+    var row = document.createElement('div');
+    row.setAttribute('class', 'row row-cols-5 grid-row');
+    row.style.marginBottom = '4px';
+    grid.appendChild(row);
+
+    for (var c = 0; c < 5 && i < alphabet.length; c++, i++) {
+      var col = document.createElement('div');
+      col.setAttribute('class', 'col');
+      col.style.display = 'flex';
+      col.style.justifyContent = 'center';
+      row.appendChild(col);
+
+      if (gaps.includes(i) && !gapUsed) {
+        --i;
+        gapUsed = true;
+        continue;
+      }
+      gapUsed = false;
+
+      var button = document.createElement('button');
+      button.innerHTML = alphabet[i][alphabetTextContentIndex];
+      button.setAttribute('class', 'button-grid glow');
+      button.setAttribute('id', 'button-grid-' + alphabet[i][1]);
+      button.setAttribute('role', 'button');
+      button.onclick = function () { checkAnswerAndMakeNewTask(this) };
+      button.addEventListener('animationstart', () => { });
+      button.addEventListener('animationiteration', () => { });
+      button.addEventListener('animationend', function (e) {
+        this.classList.remove('class-glow-correct');
+        this.classList.remove('class-glow-incorrect');
+      });
+      col.appendChild(button);
     }
-    gapUsed = false;
-
-    var button = document.createElement('button');
-    button.innerHTML = alphabet[i][0];
-    button.setAttribute('class', 'button-grid glow');
-    button.setAttribute('id', 'button-grid-' + alphabet[i][1]);
-    button.setAttribute('role', 'button');
-    button.onclick = function () { checkAnswerAndMakeNewTask(this) };
-    button.addEventListener('animationstart', () => { });
-    button.addEventListener('animationiteration', () => { });
-    button.addEventListener('animationend', function(e) {
-      this.classList.remove('class-glow-correct');
-      this.classList.remove('class-glow-incorrect');
-    });
-    col.appendChild(button);
   }
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
-//   End                    Grid generation and alphabet functions
+//                    </ Grid generation and alphabet working functions >
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 async function fetchUrlGet(url) {
@@ -99,11 +115,12 @@ async function fetchAndSet(url, docField, urlFields) {
   document.getElementById('out1')[docField] = response;
 }
 
-async function createGame() {
-  response = await fetchUrlGet('https://localhost:7073/create_game?mode=1');
+async function createGame(mode) {
+  localStorage.setItem('mode', mode);
+  createGrid(mode);
 
+  response = await fetchUrlGet(`https://localhost:7073/create_game?mode=${mode}`);
   localStorage.setItem('id', response['id']);
-
   processResponse(response);
 }
 
@@ -126,15 +143,25 @@ async function processResponse(response) {
   document.getElementById('correct-attempts-percentage').textContent = (response['attempts'] == 0 ? 100 : response['correctAttempts'] * 100 / response['attempts']).toFixed(2).toString() + '%';
 }
 
+function highlightGridButton(prevTaskAnswer) {
+  document.getElementById('button-grid-' + anythingToEnglish(localStorage.getItem('taskQuestion'))).classList.toggle('class-glow-correct');
+  if (localStorage.getItem('userAnswer') != prevTaskAnswer) {
+    document.getElementById('button-grid-' + anythingToEnglish(localStorage.getItem('userAnswer'))).classList.toggle('class-glow-incorrect');
+  }
+}
+
+document.getElementById('game-modes-button-eng-to-hir').onclick = function () {
+  createGame(1);
+};
+document.getElementById('game-modes-button-hir-to-eng').onclick = function () {
+  createGame(0);
+};
+
 //fetchAndSet('https://jsonplaceholder.typicode.com/posts/1', 'textContent', 'body');
 
 //fetchAndSet('https://localhost:7073/states', 'textContent', [0, 'id']);
 
-createGame();
+/*document.getElementById('gamefield-control-next').onclick = function () { console.log('fuck you'); };
+document.getElementById('gamefield-control-skip').onclick = function () { console.log('fuck you'); };*/
 
-function highlightGridButton(prevTaskAnswer) {
-  document.getElementById('button-grid-' + localStorage.getItem('taskQuestion')).classList.toggle('class-glow-correct');
-  if (localStorage.getItem('userAnswer') != prevTaskAnswer) {
-    document.getElementById('button-grid-' + hiraganaToEnglish(localStorage.getItem('userAnswer'))).classList.toggle('class-glow-incorrect');
-  }
-}
+createGame(1);
